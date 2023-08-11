90e060d9a644bfa725a52c52ab9128f7b5fb898e
mxerinda <mxerinda@singular.co.za>
2023-08-11 15:54:35 +0200

    End of Section 7

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,16 +1,13 @@
 import React, { ChangeEvent, useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
+import { useStore } from '../../../app/stores/store';
+import { observer } from 'mobx-react-lite';
 
-interface Props {
-    activity: Activity | undefined;
-    closeForm: () => void;
-    createOrEdit: (activity: Activity) => void;
-    submitting: boolean;
-}
 
-export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit, submitting}: Props){
-
+export default observer( function ActivityForm(){
+    const {activityStore} = useStore();
+    const {selectedActivity, closeForm, createActivity, updateActivity, loading} = activityStore;
     const initialState = selectedActivity ?? {
         id: '',
         title: '',
@@ -18,14 +15,14 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
         description: '',
         date: '',
         city: '',
-        venue: '',
+        venue: ''
 
     }
 
     const [activity, setActivity] = useState(initialState);
 
     function handleSubmit() {
-        createOrEdit(activity);
+        activity.id ? updateActivity(activity) : createActivity(activity);
     }
 
     function hanndleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
@@ -42,9 +39,9 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
                 <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={hanndleInputChange}/>
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={hanndleInputChange}/>
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={hanndleInputChange}/>
-                <Button loading={submitting} floated='right' positive type='submit' content='Submit'/>
+                <Button loading={loading} floated='right' positive type='submit' content='Submit'/>
                 <Button onClick={closeForm}floated='right' type='button' content='Cancel'/>
             </Form>
         </Segment>
     )
-}
+})
